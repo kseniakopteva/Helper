dcc0bc51e8f06a0e7854438be52690349e633f09
Ksenia <ksenija.kopteva@gmail.com>
2021-04-05 21:35:17 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.startButton = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.congratsMessage = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(199, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 41);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(94, 46);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 2;
+            // 
+            // congratsMessage
+            // 
+            this.congratsMessage.AutoSize = true;
+            this.congratsMessage.Location = new System.Drawing.Point(12, 79);
+            this.congratsMessage.Name = "congratsMessage";
+            this.congratsMessage.Size = new System.Drawing.Size(199, 13);
+            this.congratsMessage.TabIndex = 3;
+            this.congratsMessage.Text = "Congratulations! Your life has been fixed.";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(12, 103);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 141);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.congratsMessage);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label congratsMessage;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helper
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            congratsMessage.Visible = false;
+            buttonOK.Enabled = false;
+        }
+
+        private Task ProcessData(List<string> list, IProgress<ProgressReport> progress)
+        {
+            int index = 1;
+            int totalProcess = list.Count;
+            var progressReport = new ProgressReport();
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < totalProcess; i++)
+                {
+                    progressReport.PercentComplete = index++ * 100 / totalProcess;
+                    progress.Report(progressReport);
+                    Thread.Sleep(10);
+                }
+            });
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            List<string> list = new List<string>();
+            for (int i = 0; i < 1000; i++)
+                list.Add(i.ToString());
+            labelStatus.Text = "Working...";
+            var progress = new Progress<ProgressReport>();
+            progress.ProgressChanged += (o, report) =>
+              {
+                  labelStatus.Text = string.Format("Processing...{0}%", report.PercentComplete);
+                  progressBar.Value = report.PercentComplete;
+                  progressBar.Update();
+              };
+            await ProcessData(list, progress);
+            Thread.Sleep(500);
+            labelStatus.Text = "Done!";
+
+            congratsMessage.Visible = true;
+            buttonOK.Enabled = true;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        public class ProgressReport
+        {
+            public int PercentComplete { get; set; }
+        }
+
+    }
+}
